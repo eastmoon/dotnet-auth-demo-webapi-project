8befafbee68d8736356c46cf2ec04d2216e80fb4
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-04-05 20:35:07 +0800

    Add JWT service and controller

---
+++ app/WebService/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Services;
+
+namespace WebService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AuthController : ControllerBase
+    {
+        private readonly JwtService JWT;
+        public AuthController(JwtService jwt)
+        {
+            this.JWT = jwt;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public string demo()
+        {
+            return JWT.GenerateToken("demo-user");
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]/{username}")]
+        public string Token(string username)
+        {
+            Console.WriteLine("> Token call " + username);
+            return JWT.GenerateToken(username);
+        }
+
+        [HttpPost("[action]")]
+        public string Check()
+        {
+            Console.WriteLine("> Check call " + Request.Headers["Authorization"]);
+            return User.Identity.Name;
+        }
+    }
+}
