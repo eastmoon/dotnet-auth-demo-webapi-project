0ba4ab0c51ee000dbad1c9f912159dd4f244b142
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2021-04-20 14:25:31 +0800

    Add comment

--- app/WebService/Core/Authorize/AccessLevelHandler.cs
+++ app/WebService/Core/Authorize/AccessLevelHandler.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace WebService.Core.Authorize
 {
+    /// <summary>
+    /// 自定義『存取等級』授權處理函式
+    /// 針對特定授權需求，可以存在多個授權處理函式，只要此類別繼承的 AuthorizationHandler<TRequirement>，在 TRequirement 相同
+    /// 當此類別撰寫完成，請至 Startup.ConfigureServices 註冊處理函式
+    /// 參考文獻 https://docs.microsoft.com/zh-tw/aspnet/core/security/authorization/policies?view=aspnetcore-3.1#authorization-handlers
+    /// </summary>
     public class AccessLevelHandler :
          AuthorizationHandler<AccessLevelRequirement>
     {
--- app/WebService/Core/Authorize/AccessLevelRequirement.cs
+++ app/WebService/Core/Authorize/AccessLevelRequirement.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace WebService.Core.Authorize
 {
+    /// <summary>
+    /// 自定義『存取等級』授權需求
+    /// 當此類別撰寫完成，請至 Startup.ConfigureServices 註冊處理函式內，使用 services.AddAuthorization 增加授權原則
+    /// 參考文獻 https://docs.microsoft.com/zh-tw/aspnet/core/security/authorization/policies?view=aspnetcore-3.1#requirements
+    /// </summary>
     public class AccessLevelRequirement : IAuthorizationRequirement
     {
         public int AccessLevel { get; set; }
